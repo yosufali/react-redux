c5ff6a07930c4afabcdcf639846d62c8e3b1e895
Yosuf Ali <yosufali@users.noreply.github.com>
2016-11-10 17:11:06 +0000

    Connected courses page to redux store.

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,4 +1,6 @@
 import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import * as courseActions from '../actiona/courseActions';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
@@ -19,7 +21,7 @@ class CoursesPage extends React.Component {
   }
 
   onClickSave(event) {
-    alert(`Saving ${this.state.course.title}`);
+    this.props.dispatch(courseActions.createCourse(this.state.course));
   }
 
   render() {
@@ -41,4 +43,10 @@ class CoursesPage extends React.Component {
   }
 }
 
-export default CoursesPage;
+function mapStateToProps(state, ownProps) {
+  return {
+    courses: state.courses
+  };
+}
+
+export default connect(mapStateToProps)(CoursesPage);
