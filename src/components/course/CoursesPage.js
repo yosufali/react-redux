070f8f237d3277107d3ea04007030221526afa63
Yosuf Ali <yosufali@users.noreply.github.com>
2016-11-11 14:54:45 +0000

    Used bindActionCreators instead.

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,5 +1,6 @@
 import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
+import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component {
@@ -21,7 +22,7 @@ class CoursesPage extends React.Component {
   }
 
   onClickSave(event) {
-    this.props.dispatch(courseActions.createCourse(this.state.course));
+    this.props.actions.createCourse(this.state.course);
   }
 
   courseRow(course, index) {
@@ -48,15 +49,21 @@ class CoursesPage extends React.Component {
   }
 }
 
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
+};
+
 function mapStateToProps(state, ownProps) {
   return {
     courses: state.courses
   };
 }
 
-CoursesPage.propTypes = {
-  dispatch: PropTypes.func.isRequired,
-  courses: PropTypes.array.isRequired
-};
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
 
-export default connect(mapStateToProps)(CoursesPage);
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
